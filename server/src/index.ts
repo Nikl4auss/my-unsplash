3ea62b7b6f140296f4a2be1232dc48ffc9867c76
Nicolas Marinelli <nicolasmarinelli@proton.me>
2022-08-31 22:04:22 -0300

    configured cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,7 @@ import * as trpcExpress from '@trpc/server/adapters/express'
 import * as trpc from '@trpc/server'
 import { AppRouter, appRouter } from './trpc'
 import express from 'express'
-
+import cors from 'cors'
 const app = express()
 
 const createContext = ({
@@ -12,6 +12,7 @@ const createContext = ({
 
 type Context = trpc.inferAsyncReturnType<typeof createContext>
 
+app.use(cors())
 app.use('/trpc', trpcExpress.createExpressMiddleware({
   router: appRouter,
   createContext
