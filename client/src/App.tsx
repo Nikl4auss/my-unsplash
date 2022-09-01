7ea744e449cdfb78d4d7c9c7e9e16632233003af
Nicolas Marinelli <nicolasmarinelli@proton.me>
2022-08-31 22:04:10 -0300

    added and configured trpc on the client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,30 @@
-function App() {
+import { useState } from 'react'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { trpc } from './utils/trpc'
 
+function AppContent() {
+  const { data, error, isLoading } = trpc.useQuery(['hello'])
+  if (isLoading) return <p>Loading...</p>
+  if (data) return <h1>{data}</h1>
+  console.error(error)
   return (
     <h1>Hello World</h1>
+  )
+}
+
+export function App() {
+
+  const [queryClient] = useState(() => new QueryClient())
+  const [trpcClient] = useState(() => trpc.createClient({
+    url: 'http://localhost:3000/trpc'
+  }))
+
+  return (
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        <AppContent />
+      </QueryClientProvider>
+    </trpc.Provider>
   )
 }
 
