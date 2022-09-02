36abde1877c0d2d8f1f59b2e5f5748bb6a82ec95
Nicolas Marinelli <nicolasmarinelli@proton.me>
2022-09-02 16:16:15 -0300

    Added the NavBar component

---
+++ client/src/components/NavBar.tsx
@@ -0,0 +1,14 @@
+function NavBar() {
+  return (
+    <nav>
+      <div>
+        <h1>My unsplash</h1>
+        <p>devchallenges.io</p>
+      </div>
+      <input type="text" placeholder="Search by name" />
+      <button type="button">Add a photo</button>
+    </nav>
+  )
+}
+
+export default NavBar
